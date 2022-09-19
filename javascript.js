bf803ed2054546eeeb4cb6343ed39e9a346b2dfe
LeeKiller9 <112848883+LeeKiller9@users.noreply.github.com>
2022-09-20 03:42:44 +0700

    reup html js files

--- javascript.js
+++ javascript.js
@@ -1,38 +1,38 @@
-let row = 4 //+(prompt("Enter Row Of Board:"));
-let col = 6 //+(prompt("Enter Column Of Board:"));
+let row = +(prompt("Enter Row Of Board:"));
+let col = +(prompt("Enter Column Of Board:"));
 let b = document.getElementById("result");
 let arrayRow = [];
 let board = [];
 let data = "";
+
 for (let i = 0; i < col; i++) {
     arrayRow[i] = 0;
 }
-for (let i = 0; i < row; i++) {
-    board[i] = arrayRow;
+
+for (let i = 0; i < row; i++) {//phai co dong lenh nay` de khoi tao array board 2 chieu
+    board[i] = [];
 }
 
 for (let i = 0; i < row; i++) {
-    data += "<br/>";
     for (let j = 0; j < col; j++) {
+        board[i][j] = arrayRow[j];
         data += board[i][j] + "&nbsp;&nbsp;";
     }
+    data += "<br/>";
 }
 b.innerHTML = data;
 
-console.log(row)
-console.log(board);
-
 function displayBoard() {
-    let posX = 1//+(prompt("X: "));
-    let posY = 1//+(prompt("Y: "));
+    let posX = +(prompt("X: "));
+    let posY = +(prompt("Y: "));
     data = "";
 
     for (let i = 0; i < row; i++) {
         for (let j = 0; j < col; j++) {
             if (i === posX && j === posY) {
-                data += board[posX][posY] = "x";
+                board[posX][posY] = "x";
             }
-            data += board[i][j]; //+ "&nbsp;&nbsp";
+            data += board[i][j] + "&nbsp;&nbsp";
         }
         data += "<br/>";
     }
